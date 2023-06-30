81b9488709593ba377c95ef43ac25aabcf6691a6
AaronPB <arp629@gmail.com>
2023-06-30 14:59:22 +0200

    Modify some plot values and line colors

--- matlab/csv_data_plotter.m
+++ matlab/csv_data_plotter.m
@@ -9,7 +9,7 @@
 % - 3 subfigures for IMUs angular velocity data
 
 %% IMPORT DATA
-data = readtable('../pruebas/TestViktorApoyos.csv', 'Delimiter', ',');
+data = readtable('../ensayos_2023_06_30/EnsayoRapida.csv', 'Delimiter', ',');
 
 % Transform timestamp to seconds
 timestamps = data.timestamp;
@@ -28,10 +28,16 @@ if ismember('P1_LoadCell_Z_1', data.Properties.VariableNames) && ...
     resultant_Z1 = data.P1_LoadCell_Z_1 + data.P1_LoadCell_Z_2 + data.P1_LoadCell_Z_3 + data.P1_LoadCell_Z_4;
 
     subplot(3, 1, 1);
-    plot(timeIncrements, [loadCell_Z1,resultant_Z1], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_Z1(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z1(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z1(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z1(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_Z1, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P1\_LoadCell\_Z');
     legend('P1\_LoadCell\_Z\_1', 'P1\_LoadCell\_Z\_2', 'P1\_LoadCell\_Z\_3', 'P1\_LoadCell\_Z\_4', 'RESULTANTE');
 else
@@ -48,10 +54,16 @@ if ismember('P1_LoadCell_X_1', data.Properties.VariableNames) && ...
     resultant_X1 = -data.P1_LoadCell_X_1 + data.P1_LoadCell_X_2 + data.P1_LoadCell_X_3 - data.P1_LoadCell_X_4;
 
     subplot(3, 1, 2);
-    plot(timeIncrements, [loadCell_X1,resultant_X1], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_X1(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X1(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X1(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X1(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_X1, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P1\_LoadCell\_X');
     legend('P1\_LoadCell\_X\_1', 'P1\_LoadCell\_X\_2', 'P1\_LoadCell\_X\_3', 'P1\_LoadCell\_X\_4', 'RESULTANTE');
     
@@ -69,10 +81,16 @@ if ismember('P1_LoadCell_Y_1', data.Properties.VariableNames) && ...
     resultant_Y1 = -data.P1_LoadCell_Y_1 - data.P1_LoadCell_Y_2 + data.P1_LoadCell_Y_3 + data.P1_LoadCell_Y_4;
     
     subplot(3, 1, 3);
-    plot(timeIncrements, [loadCell_Y1,resultant_Y1], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_Y1(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y1(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y1(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y1(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_Y1, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P1\_LoadCell\_Y');
     legend('P1\_LoadCell\_Y\_1', 'P1\_LoadCell\_Y\_2', 'P1\_LoadCell\_Y\_3', 'P1\_LoadCell\_Y\_4', 'RESULTANTE');
 else
@@ -92,10 +110,16 @@ if ismember('P2_LoadCell_Z_1', data.Properties.VariableNames) && ...
     resultant_Z2 = data.P2_LoadCell_Z_1 + data.P2_LoadCell_Z_2 + data.P2_LoadCell_Z_3 + data.P2_LoadCell_Z_4;
 
     subplot(3, 1, 1);
-    plot(timeIncrements, [loadCell_Z2,resultant_Z2], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_Z2(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z2(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z2(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Z2(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_Z2, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P2\_LoadCell\_Z');
     legend('P2\_LoadCell\_Z\_1', 'P2\_LoadCell\_Z\_2', 'P2\_LoadCell\_Z\_3', 'P2\_LoadCell\_Z\_4','RESULTANTE');
 else
@@ -112,10 +136,16 @@ if ismember('P2_LoadCell_X_1', data.Properties.VariableNames) && ...
     resultant_X2 = -data.P2_LoadCell_X_1 + data.P2_LoadCell_X_2 + data.P2_LoadCell_X_3 - data.P2_LoadCell_X_4;
 
     subplot(3, 1, 2);
-    plot(timeIncrements, [loadCell_X2,resultant_X2], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_X2(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X2(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X2(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_X2(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_X2, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P2\_LoadCell\_X');
     legend('P2\_LoadCell\_X\_1', 'P2\_LoadCell\_X\_2', 'P2\_LoadCell\_X\_3', 'P2\_LoadCell\_X\_4','RESULTANTE');
 else
@@ -132,10 +162,16 @@ if ismember('P2_LoadCell_Y_1', data.Properties.VariableNames) && ...
     resultant_Y2 = -data.P2_LoadCell_Y_1 - data.P2_LoadCell_Y_2 + data.P2_LoadCell_Y_3 + data.P2_LoadCell_Y_4;
     
     subplot(3, 1, 3);
-    plot(timeIncrements, [loadCell_Y2,resultant_Y2], 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, loadCell_Y2(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y2(:, 2), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y2(:, 3), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, loadCell_Y2(:, 4), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, resultant_Y2, 'k-', 'LineWidth', 2.5);
+    hold off;
     grid on;
     xlabel('Time (s)');
-    ylabel('Force (N)');
+    ylabel('Weight (kg)');
     title('P2\_LoadCell\_Y');
     legend('P2\_LoadCell\_Y\_1', 'P2\_LoadCell\_Y\_2', 'P2\_LoadCell\_Y\_3', 'P2\_LoadCell\_Y\_4','RESULTANTE');
 
@@ -150,7 +186,12 @@ if ismember('Encoder_Z_1', data.Properties.VariableNames) && ...
 
     encoders = [data.Encoder_Z_1, data.Encoder_Z_2];
 
-    plot(timeIncrements, encoders, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, encoders(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, encoders(:, 2), 'b-', 'LineWidth', 1.5);
+    hold off;
+
+    % plot(timeIncrements, encoders, 'LineWidth', 1.5);
     grid on;
     xlabel('Time (s)');
     ylabel('Distance (mm)');
@@ -175,7 +216,12 @@ if ismember('IMU_Leg_Right_qw', data.Properties.VariableNames) && ...
     imu_1_eul_deg = rad2deg(imu_1_eul);
     
     subplot(3, 1, 1);
-    plot(timeIncrements, imu_1_eul_deg, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_1_eul_deg(:, 1), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_1_eul_deg(:, 2), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_1_eul_deg(:, 3), 'k-', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_1_eul_deg, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angle (º)');
     title('IMU\_1');
@@ -196,7 +242,12 @@ if ismember('IMU_Thigh_Right_qw', data.Properties.VariableNames) && ...
     imu_2_eul_deg = rad2deg(imu_2_eul);
     
     subplot(3, 1, 2);
-    plot(timeIncrements, imu_2_eul_deg, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_2_eul_deg(:, 1), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_2_eul_deg(:, 2), 'b--', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_2_eul_deg(:, 3), 'k-', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_2_eul_deg, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angle (º)');
     title('IMU\_2');
@@ -217,7 +268,12 @@ if ismember('IMU_UpperTrunk_Right_qw', data.Properties.VariableNames) && ...
     imu_3_eul_deg = rad2deg(imu_3_eul);
     
     subplot(3, 1, 3);
-    plot(timeIncrements, imu_3_eul_deg, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_3_eul_deg(:, 1), 'r--', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_3_eul_deg(:, 2), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_3_eul_deg(:, 3), 'k--', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_3_eul_deg, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angle (º)');
     title('IMU\_3');
@@ -237,7 +293,12 @@ if ismember('IMU_Leg_Right_wx', data.Properties.VariableNames) && ...
     imu_1_w = [data.IMU_Leg_Right_wx, data.IMU_Leg_Right_wy, data.IMU_Leg_Right_wz];
 
     subplot(3, 1, 1);
-    plot(timeIncrements, imu_1_w, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_1_w(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_1_w(:, 2), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_1_w(:, 3), 'k-', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_1_w, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angular velocity (rad/s)');
     title('IMU\_1');
@@ -254,7 +315,12 @@ if ismember('IMU_Thigh_Right_wx', data.Properties.VariableNames) && ...
     imu_2_w = [data.IMU_Thigh_Right_wx, data.IMU_Thigh_Right_wy, data.IMU_Thigh_Right_wz];
 
     subplot(3, 1, 2);
-    plot(timeIncrements, imu_2_w, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_2_w(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_2_w(:, 2), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_2_w(:, 3), 'k-', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_2_w, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angular velocity (rad/s)');
     title('IMU\_2');
@@ -271,7 +337,12 @@ if ismember('IMU_UpperTrunk_Right_wx', data.Properties.VariableNames) && ...
     imu_3_w = [data.IMU_UpperTrunk_Right_wx, data.IMU_UpperTrunk_Right_wy, data.IMU_UpperTrunk_Right_wz];
 
     subplot(3, 1, 3);
-    plot(timeIncrements, imu_3_w, 'LineWidth', 1.5);
+    hold on;
+    plot(timeIncrements, imu_3_w(:, 1), 'r-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_3_w(:, 2), 'b-', 'LineWidth', 1.5);
+    plot(timeIncrements, imu_3_w(:, 3), 'k-', 'LineWidth', 1.5);
+    hold off;
+    % plot(timeIncrements, imu_3_w, 'LineWidth', 1.5);
     xlabel('Time (s)');
     ylabel('Angular velocity (rad/s)');
     title('IMU\_3');
