c597cecde6b9df37dfa1b6b7c3b757da4913fd29
AaronPB <arp629@gmail.com>
2023-10-24 11:04:20 +0200

    Empty enums init

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -1,5 +1,1 @@
-__all__ = ['configPaths', 'sensorsParams', 'sensorStatus']
-# Deprecated for older python versions
-from enums.configPaths import ConfigPaths
-from enums.sensorParams import SensorParams
-from enums.sensorStatus import SensorStatus
+
