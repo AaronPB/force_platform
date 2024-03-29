0c7d38f5599f1cb331d6e5b675c7a99cd97eca09
AaronPB <arp629@gmail.com>
2024-03-29 16:23:27 +0100

    Update config enum paths and separate/rename sensor and sensor group params

--- src/enums/sensorParams.py
+++ src/enums/sensorParams.py
@@ -1,21 +1,31 @@
 from enum import Enum
 
 
-class SensorParams(Enum):
-    # Internal config params
+# Sensor params
+class SParams(Enum):
+    # Config sections
+    CONNECTION_SECTION = "connection"
+    CALIBRATION_SECTION = "calibration"
+    PROPERTIES_SECTION = "properties"
+
+    # Config params
     NAME = "name"
+    TYPE = "type"
     CHANNEL = "channel"
     SERIAL = "serial"
-    READ = "read_data"
+    SLOPE = "slope"
+    INTERCEPT = "intercept"
     INITIAL_POS = "initial_position"
-    SLOPE = "m"
-    INTERCEPT = "b"
-
-    # Internal config sections
-    CALIBRATION_SECTION = "calibration_data"
-    PROPERTIES_SECTION = "properties"
 
     # Handler additional params
     STATUS = "status"
     SENSOR = "sensor"
     VALUE = "value"
+
+
+# Sensor group params
+class SGParams(Enum):
+    NAME = "name"
+    TYPE = "type"
+    READ = "read"
+    SENSOR_LIST = "sensor_list"
