8a56dced0577601df5d0f8ab977e7353a09aa473
AaronPB <arp629@gmail.com>
2024-04-01 13:29:41 +0200

    WIP New sensor group load process

--- src/sensorLoader.py
+++ src/sensorLoader.py
@@ -6,121 +6,130 @@ from src.handlers.sensorGroup import SensorGroup
 from src.handlers.sensor import Sensor, Driver
 from src.handlers.drivers import PhidgetLoadCell, PhidgetEncoder, TaoboticsIMU
 from src.enums.configPaths import ConfigPaths as CfgPaths
-from src.enums.sensorParams import SensorParams as SParams
+from src.enums.sensorParams import SParams, SGParams
+from src.enums.sensorTypes import STypes, SGTypes
+
+# Required param keys for sensor handlers
+group_keys = [
+    SGParams.NAME,
+    SGParams.TYPE,
+    SGParams.READ,
+    SGParams.SENSOR_LIST,
+]
+sensor_keys = [SParams.NAME, SParams.TYPE, SParams.READ, SParams.CONNECTION_SECTION]
+loadcell_keys = [SParams.SERIAL, SParams.CHANNEL]
+encoder_keys = [
+    SParams.SERIAL,
+    SParams.CHANNEL,
+    SParams.INITIAL_POS,
+]
+taobotics_keys = [SParams.NAME, SParams.SERIAL]
 
 
 class SensorLoader:
-    def __init__(self, config_mngr: ConfigManager) -> None:
-        # Global values
-        self.config_mngr = config_mngr
+    def __init__(self) -> None:
+        self.config_sensors: dict = {}
 
-        # Required config keys for each sensor group
-        self.required_keys_loadcells = [
-            SParams.NAME,
-            SParams.READ,
-            SParams.SERIAL,
-            SParams.CHANNEL,
-            SParams.CALIBRATION_SECTION,
-        ]
-        self.required_keys_encoders = [
-            SParams.NAME,
-            SParams.READ,
-            SParams.SERIAL,
-            SParams.CHANNEL,
-            SParams.CALIBRATION_SECTION,
-            SParams.INITIAL_POS,
-        ]
-        self.required_keys_taobotics = [SParams.NAME, SParams.READ, SParams.SERIAL]
+        self.sensor_groups: list[SensorGroup] = []
+        self.platform_groups: list[SensorGroup] = []
+        self.encoder_groups: list[SensorGroup] = []
+        self.imu_groups: list[SensorGroup] = []
 
-    def loadHandlers(self):
-        self.sensor_group_platform1 = self.setSensorGroup(
-            "Platform 1",
-            CfgPaths.PHIDGET_P1_LOADCELL_CONFIG_SECTION,
-            self.required_keys_loadcells,
-            PhidgetLoadCell,
-        )
-        self.sensor_group_platform2 = self.setSensorGroup(
-            "Platform 2",
-            CfgPaths.PHIDGET_P2_LOADCELL_CONFIG_SECTION,
-            self.required_keys_loadcells,
-            PhidgetLoadCell,
-        )
-        self.sensor_group_encoders = self.setSensorGroup(
-            "Barbell encoders",
-            CfgPaths.PHIDGET_ENCODER_CONFIG_SECTION,
-            self.required_keys_encoders,
-            PhidgetEncoder,
-        )
-        self.sensor_group_imus = self.setSensorGroup(
-            "Body IMUs",
-            CfgPaths.TAOBOTICS_IMU_CONFIG_SECTION,
-            self.required_keys_taobotics,
-            TaoboticsIMU,
-        )
-        self.ref_sensor = Sensor()
-        self.loadSensor(
-            self.ref_sensor,
-            "REF",
-            CfgPaths.CALIBRATION_CONFIG_SECTION.value,
-            self.required_keys_loadcells,
-            PhidgetLoadCell,
-        )
+        self.loadcell_calib_ref: Sensor = None
+        self.platform_calib_ref: Sensor = None
 
-    def setSensorGroup(
-        self,
-        group_name: str,
-        config_section: CfgPaths,
-        required_keys: list,
-        sensor_driver: Driver,
-    ) -> SensorGroup:
-        sensor_group = SensorGroup(group_name)
-        for sensor_id in self.config_mngr.getConfigValue(config_section.value):
-            sensor = Sensor()
-            config_path = config_section.value + "." + sensor_id
-            setup = self.loadSensor(
-                sensor, sensor_id, config_path, required_keys, sensor_driver
-            )
-            if not setup:
-                logger.warning(f"Could not load sensor in config path {config_path}!")
+    def setup(self, config_mngr: ConfigManager) -> None:
+        self.config_sensors = config_mngr.getConfigValue(
+            CfgPaths.SENSORS_SECTION.value, {}
+        )
+        config_groups = config_mngr.getConfigValue(
+            CfgPaths.SENSOR_GROUPS_SECTION.value, {}
+        )
+        loadcell_calib_id = config_mngr.getConfigValue(
+            CfgPaths.CALIBRATION_LOADCELL_SENSOR.value, {}
+        )
+        platform_calib_id = config_mngr.getConfigValue(
+            CfgPaths.CALIBRATION_PLATFORM_SENSOR.value, {}
+        )
+        self.loadSensorGroups(config_groups)
+        self.loadcell_calib_ref = self.loadSensor(loadcell_calib_id)
+        self.platform_calib_ref = self.loadSensor(platform_calib_id)
+
+    def loadSensorGroups(self, config_groups: dict) -> None:
+        if not config_groups:
+            logger.error("No sensor groups found in config!")
+            return
+        if not self.config_sensors:
+            logger.error("No sensors found in config!")
+            return
+        for group_id in config_groups:
+            sensor_group = self.loadSensorGroup(group_id, config_groups[group_id])
+            if sensor_group is None:
                 continue
-            sensor_group.addSensor(sensor)
+            # TODO check group type (for certain groups such as platforms)
+            # TODO add group to list
+
+    def loadSensorGroup(self, id: str, content: dict) -> SensorGroup:
+        if content is None:
+            logger.warning(f"Sensor group {id} is empty! Not loaded.")
+            return None
+        if not all(key.value in content.keys() for key in group_keys):
+            logger.warning(
+                f"Sensor group {id} does not have the required keys! Not loaded."
+            )
+            return None
+        if not content[SGParams.SENSOR_LIST.value]:
+            logger.warning(f"Sensor group {id} has an empty sensor list! Not loaded.")
+            return None
+        sensor_group = SensorGroup(id, content[SGParams.NAME.value])
+        # Load all sensors for this sensor group
+        for sensor_id in content[SGParams.SENSOR_LIST]:
+            sensor = self.loadSensor(sensor_id)
+            if sensor is not None:
+                sensor_group.addSensor(sensor)
+        # Check if any sensor has been loaded
+        if sensor_group.getSize() == 0:
+            logger.error(f"Sensor group {id} is empty. Not loaded.")
+            return None
         return sensor_group
 
-    def loadSensor(
-        self,
-        sensor: Sensor,
-        id: str,
-        config_path: str,
-        required_keys: list,
-        driver: Driver,
-    ) -> bool:
-        sensor_params = self.config_mngr.getConfigValue(config_path, None)
-        if sensor_params is None:
-            return False
-        if not all(key.value in sensor_params.keys() for key in required_keys):
-            return False
-        sensor.setup(id, sensor_params, driver)
-        return True
+    def loadSensor(self, id: str) -> Sensor:
+        if not id in self.config_sensors:
+            logger.warning(
+                f"Did not found sensor {id} in sensors config section. Not loaded."
+            )
+            return None
+        content = self.config_sensors[id]
+        # TODO Now sensor params needs to have a generic style.
+        # Following sections are required: name, type and connection.
+        # Optional sections: calibration and properties.
+        if not all(key.value in content.keys() for key in sensor_keys):
+            logger.warning(f"Sensor {id} does not have the required keys! Not loaded.")
+            return None
+        if content[SParams.TYPE.value] not in STypes:
+            logger.warning(
+                f"Sensor {id} does not have a valid sensor type! Not loaded."
+            )
+            return None
+        # TODO sensor params
+        sensor = Sensor()
+        sensor.setup(id, {}, STypes[content[SParams.TYPE.value]].value)
+        return sensor
 
-    def getSensorGroups(self) -> list:
-        return [
-            self.sensor_group_platform1,
-            self.sensor_group_platform2,
-            self.sensor_group_encoders,
-            self.sensor_group_imus,
-        ]
+    def getGroups(self) -> list:
+        return self.sensor_groups
 
-    def getSensorGroupPlatform1(self) -> SensorGroup:
-        return self.sensor_group_platform1
+    def getPlatformGroups(self) -> SensorGroup:
+        return self.platform_groups
 
-    def getSensorGroupPlatform2(self) -> SensorGroup:
-        return self.sensor_group_platform2
+    def getEncoderGroups(self) -> SensorGroup:
+        return self.encoder_groups
 
-    def getSensorGroupEncoders(self) -> SensorGroup:
-        return self.sensor_group_encoders
+    def getIMUGroups(self) -> SensorGroup:
+        return self.imu_groups
 
-    def getSensorGroupIMUs(self) -> SensorGroup:
-        return self.sensor_group_imus
+    def getSensorCalibRef(self) -> Sensor:
+        pass
 
-    def getRefSensor(self) -> Sensor:
-        return self.ref_sensor
+    def getPlatformCalibRef(self) -> Sensor:
+        pass
