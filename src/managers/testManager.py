18ccdb14a66392d51be68b3afe6b83c4f9869d67
AaronPB <arp629@gmail.com>
2024-04-03 11:26:51 +0200

    Remove sensor getters, managers dependencies and clean imports

--- src/managers/testManager.py
+++ src/managers/testManager.py
@@ -2,37 +2,20 @@
 
 import time
 from loguru import logger
-from src.managers.configManager import ConfigManager
-from src.managers.sensorManager import SensorManager
-from src.handlers import SensorGroup
-from src.enums.configPaths import ConfigPaths as CfgPaths
-from src.enums.sensorParams import SensorParams as SParams
+from src.handlers.sensorGroup import SensorGroup
 
 
 class TestManager:
     __test__ = False
 
-    def __init__(
-        self, config_mngr: ConfigManager, sensor_manager: SensorManager
-    ) -> None:
-        # Global values
-        self.config_mngr = config_mngr
-        self.sensor_mngr = sensor_manager
-        self.sensors_connected = False
-        self.test_times = []
+    def __init__(self) -> None:
+        self.sensor_groups: list[SensorGroup]
+        self.sensors_connected: bool = False
+        self.test_times: list = []
 
-    # Sensor setters and getters
-
-    def getP1SensorStatus(self) -> dict:
-        return self.group_platform1.getGroupInfo()
-
-    def getP2SensorStatus(self) -> dict:
-        return self.group_platform2.getGroupInfo()
-
-    def getOthersSensorStatus(self) -> dict:
-        encoder_dict = self.group_encoders.getGroupInfo()
-        imu_dict = self.group_imus.getGroupInfo()
-        return {**encoder_dict, **imu_dict}
+    # Setters and getters
+    def setSensorGroups(self, sensor_groups: list[SensorGroup]) -> None:
+        self.sensor_groups = sensor_groups
 
     def getSensorConnected(self) -> bool:
         return self.sensors_connected
@@ -43,7 +26,7 @@ class TestManager:
     # Test methods
     def checkConnection(self) -> bool:
         connection_results_list = [
-            handler.checkConnections() for handler in self.sensor_mngr.getGroups()
+            handler.checkConnections() for handler in self.sensor_groups
         ]
         self.sensors_connected = any(connection_results_list)
         return self.sensors_connected
@@ -51,15 +34,13 @@ class TestManager:
     def testStart(self, test_name: str) -> None:
         logger.info(f"Starting test: {test_name}")
         self.test_times = []
-        [handler.clearSensorValues() for handler in self.sensor_mngr.getGroups()]
-        [handler.start() for handler in self.sensor_mngr.getGroups()]
+        [handler.clearValues() for handler in self.sensor_groups]
+        [handler.start() for handler in self.sensor_groups]
 
     def testRegisterValues(self) -> None:
         self.test_times.append(round(time.time() * 1000))
-        [handler.register() for handler in self.sensor_mngr.getGroups()]
+        [handler.register() for handler in self.sensor_groups]
 
     def testStop(self, test_name: str) -> None:
         logger.info(f"Finish test: {test_name}")
-        [handler.stop() for handler in self.sensor_mngr.getGroups()]
-        # Save modified intercepts in config
-        self.config_mngr.saveConfig()
+        [handler.stop() for handler in self.sensor_groups]
