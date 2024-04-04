3e09beb2818474f304b6d580f45d8234d5b4b845
AaronPB <arp629@gmail.com>
2024-04-04 19:25:52 +0200

    Set GUI style in dark mode, to avoid issues with icon colors

--- main.py
+++ main.py
@@ -2,12 +2,32 @@
 
 import sys
 from src.qtUIs.mainWindow import MainMenu
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
+
+
+def palette() -> QtGui.QPalette:
+    dark_palette = QtGui.QPalette()
+    dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
+    dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(QtGui.QPalette.ToolTipText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(QtGui.QPalette.Text, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(QtGui.QPalette.BrightText, QtGui.QColor(255, 0, 0))
+    dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.HighlightedText, QtGui.QColor(0, 0, 0))
+    return dark_palette
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setStyleSheet(open("src/qtUIs/style.qss").read())
+    app.setStyle("Fusion")
+    app.setPalette(palette())
     window = MainMenu()
     window.show()
     sys.exit(app.exec())
