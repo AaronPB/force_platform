f9af4272994e80f47b3c3f0524e4caa9e174a270
AaronPB <arp629@gmail.com>
2024-05-01 20:55:48 +0200

    Update UI inits with ImagePaths

--- src/qtUIs/mainWindow.py
+++ src/qtUIs/mainWindow.py
@@ -1,44 +1,31 @@
 # -*- coding: utf-8 -*-
 
-import os
-from PySide6 import QtWidgets, QtGui, QtCore
+from PySide6 import QtWidgets, QtGui
 from src.qtUIs.mainUI import MainUI
 from src.qtUIs.calibrationUI import CalibrationUI
 from src.managers.configManager import ConfigManager
 from src.managers.sensorManager import SensorManager
+from src.enums.uiResources import ImagePaths
 
 
 class MainMenu(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.images_folder = os.path.join(
-            os.path.dirname(__file__), "..", "..", "images"
-        )
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Force platform reader")
-        self.setWindowIcon(
-            QtGui.QIcon(os.path.join(self.images_folder, "window_icon.ico"))
-        )
+        self.setWindowIcon(QtGui.QIcon(ImagePaths.WINDOW_ICON.value))
         self.setGeometry(100, 100, 1920, 1080)
 
         stacked_widget = QtWidgets.QStackedWidget()
-        logo_image_path = os.path.join(self.images_folder, "project_ui_logo.svg")
-        platform_image_path = os.path.join(self.images_folder, "platform.png")
         config_manager = ConfigManager()
         sensor_manager = SensorManager()
 
         # Define UIs and connect signals
-        self.mainUI = MainUI(
-            stacked_widget, config_manager, sensor_manager, logo_image_path
-        )
+        self.mainUI = MainUI(stacked_widget, config_manager, sensor_manager)
         self.calibrationUI = CalibrationUI(
-            stacked_widget,
-            config_manager,
-            sensor_manager,
-            logo_image_path,
-            platform_image_path,
+            stacked_widget, config_manager, sensor_manager
         )
         self.mainUI.close_menu.connect(self.close)
         stacked_widget.currentChanged.connect(self.stackChangeHandler)
