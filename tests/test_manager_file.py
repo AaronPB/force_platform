37533a1d4f82f01f6f14a945560aeebbd7838ac6
AaronPB <arp629@gmail.com>
2024-05-24 20:26:40 +0200

    Update test methods for file manager

--- tests/test_manager_file.py
+++ tests/test_manager_file.py
@@ -2,6 +2,7 @@
 
 import pytest
 import os
+import glob
 import pandas as pd
 from src.managers.fileManager import FileManager
 from pytest import MonkeyPatch
@@ -12,20 +13,21 @@ from pytest import MonkeyPatch
 
 class ConfigManagerMock:
     def __init__(self) -> None:
-        self.set_cfg_value_calls = 0
+        pass
 
     def setConfigValue(self, config_path: str, value) -> None:
-        self.set_cfg_value_calls += 1
+        pass
 
     def getConfigValue(self, config_path: str, default_value):
         return default_value
 
 
 @pytest.fixture
-def test_manager() -> FileManager:
-    test_mngr = FileManager(cfg_mngr=ConfigManagerMock())
-    test_mngr.file_path = os.path.dirname(__file__)
-    return test_mngr
+def file_manager() -> FileManager:
+    file_manager = FileManager()
+    file_manager.setup(config_manager=ConfigManagerMock())
+    file_manager.file_path = os.path.dirname(__file__)
+    return file_manager
 
 
 @pytest.fixture
@@ -40,76 +42,96 @@ def dataframe() -> pd.DataFrame:
 # Tests
 
 
-def test_check_no_duplicated_file(test_manager: FileManager) -> None:
-    new_name = "New test"
-    test_name = test_manager.checkDuplicatedName(new_name)
-    assert test_name == new_name
+def test_check_file_suffix(monkeypatch: MonkeyPatch, file_manager: FileManager) -> None:
+    glob_patterns = {
+        os.path.join(file_manager.file_path, "Test.*"): [
+            "Test.csv",
+            "Test_RAW.csv",
+        ],
+        os.path.join(file_manager.file_path, "Test_1.*"): [
+            "Test_1.csv",
+            "Test_RAW_1.csv",
+        ],
+        os.path.join(file_manager.file_path, "Test_2.*"): [
+            "Test_2.csv",
+            "Test_RAW_2.csv",
+        ],
+        os.path.join(file_manager.file_path, "Test_3.*"): [],
+    }
+
+    def mock_glob(pattern):
+        return glob_patterns.get(pattern, [])
+
+    monkeypatch.setattr(glob, "glob", mock_glob)
+    file_manager.checkFileName("Test")
+    assert file_manager.getFileName() == "Test_3"
 
 
-def test_check_duplicated_file(
-    monkeypatch: MonkeyPatch, test_manager: FileManager
+def test_check_file_suffix_different_forms(
+    monkeypatch: MonkeyPatch, file_manager: FileManager
 ) -> None:
-    os_exists_mock = [True, True, True, False]
+    glob_patterns = {
+        os.path.join(file_manager.file_path, "Test.*"): ["Test.csv", "Test_RAW.csv"],
+        os.path.join(file_manager.file_path, "Test_1.*"): ["Test_RAW_1.csv"],
+        os.path.join(file_manager.file_path, "Test_2.*"): ["Test_2.pk1"],
+        os.path.join(file_manager.file_path, "Test_3.*"): ["Test_3.txt"],
+        os.path.join(file_manager.file_path, "Test_4.*"): [],
+    }
 
-    def mock_exists(path):
-        os_exists_status = os_exists_mock[0]
-        os_exists_mock.pop(0)
-        return os_exists_status
+    def mock_glob(pattern):
+        return glob_patterns.get(pattern, [])
 
-    monkeypatch.setattr(os.path, "exists", mock_exists)
-    test_manager.checkDuplicatedName("Test")
-    result = test_manager.getFileName()
-    assert result == "Test_3"
+    monkeypatch.setattr(glob, "glob", mock_glob)
+    file_manager.checkFileName("Test")
+    assert file_manager.getFileName() == "Test_4"
 
 
-def test_file_name_setter(test_manager: FileManager) -> None:
+def test_file_name_setter(file_manager: FileManager) -> None:
     new_name = "New test"
-    test_manager.setFileName(new_name)
-    config_manager: ConfigManagerMock = test_manager.cfg_mngr
-    assert config_manager.set_cfg_value_calls == 1
+    file_manager.setFileName(new_name)
+    assert file_manager.getFileName() == new_name
 
 
-def test_file_name_setter_equal(test_manager: FileManager) -> None:
+def test_file_name_setter_equal(file_manager: FileManager) -> None:
     new_name = "Test"
-    test_manager.setFileName(new_name)
-    config_manager: ConfigManagerMock = test_manager.cfg_mngr
-    assert config_manager.set_cfg_value_calls == 0
+    file_manager.setFileName(new_name)
+    assert file_manager.getFileName() == new_name
 
 
-def test_filepath_name_setter(test_manager: FileManager) -> None:
-    test_manager.setFilePath("/test/path")
-    config_manager: ConfigManagerMock = test_manager.cfg_mngr
-    assert config_manager.set_cfg_value_calls == 1
+def test_filepath_name_setter(file_manager: FileManager) -> None:
+    test_path = "/test/path"
+    file_manager.setFilePath(test_path)
+    assert file_manager.getFilePath() == test_path
 
 
-def test_file_name_check(test_manager: FileManager) -> None:
-    test_manager.checkFileName()
-    assert test_manager.getFileName() == "Test"
+def test_file_name_check(file_manager: FileManager) -> None:
+    file_manager.checkFileName()
+    assert file_manager.getFileName() == "Test"
 
 
-def test_file_name_getter(test_manager: FileManager) -> None:
-    assert test_manager.getFileName() == "Test"
+def test_file_name_getter(file_manager: FileManager) -> None:
+    assert file_manager.getFileName() == "Test"
 
 
-def test_file_path_getter(test_manager: FileManager) -> None:
-    assert os.path.samefile(test_manager.getFilePath(), os.path.dirname(__file__))
+def test_file_path_getter(file_manager: FileManager) -> None:
+    assert os.path.samefile(file_manager.getFilePath(), os.path.dirname(__file__))
 
 
-def test_file_path_exists(test_manager: FileManager) -> None:
-    assert test_manager.getPathExists()
+def test_file_path_exists(file_manager: FileManager) -> None:
+    assert file_manager.getPathExists()
 
 
-def test_file_path_not_exists(test_manager: FileManager) -> None:
-    test_manager.setFilePath("/test/non/existing/path", check_name=False)
-    assert not test_manager.getPathExists()
+def test_file_path_not_exists(file_manager: FileManager) -> None:
+    file_manager.setFilePath("/test/non/existing/path")
+    assert not file_manager.getPathExists()
 
 
 def test_file_save_csv_dataframe(
-    test_manager: FileManager, dataframe: pd.DataFrame
+    file_manager: FileManager, dataframe: pd.DataFrame
 ) -> None:
-    test_manager.saveDataToCSV(df=dataframe)
+    file_manager.saveDataToCSV(df=dataframe)
     total_path = os.path.join(
-        test_manager.getFilePath(), test_manager.getFileName() + ".csv"
+        file_manager.getFilePath(), file_manager.getFileName() + ".csv"
     )
     file_exists = os.path.exists(total_path)
     if file_exists:
@@ -118,23 +140,23 @@ def test_file_save_csv_dataframe(
 
 
 def test_file_save_failure_csv_dataframe(
-    test_manager: FileManager, dataframe: pd.DataFrame
+    file_manager: FileManager, dataframe: pd.DataFrame
 ) -> None:
-    test_manager.setFilePath("/test/non/existing/path", check_name=False)
-    test_manager.saveDataToCSV(df=dataframe)
+    file_manager.setFilePath("/test/non/existing/path")
+    file_manager.saveDataToCSV(df=dataframe)
     total_path = os.path.join(
-        test_manager.getFilePath(), test_manager.getFileName() + ".csv"
+        file_manager.getFilePath(), file_manager.getFileName() + ".csv"
     )
     file_exists = os.path.exists(total_path)
     assert not file_exists
 
 
 def test_file_save_binary_dataframe(
-    test_manager: FileManager, dataframe: pd.DataFrame
+    file_manager: FileManager, dataframe: pd.DataFrame
 ) -> None:
-    test_manager.saveDataToBinary(df=dataframe)
+    file_manager.saveDataToBinary(df=dataframe)
     total_path = os.path.join(
-        test_manager.getFilePath(), test_manager.getFileName() + ".pk1"
+        file_manager.getFilePath(), file_manager.getFileName() + ".pk1"
     )
     file_exists = os.path.exists(total_path)
     if file_exists:
@@ -143,12 +165,12 @@ def test_file_save_binary_dataframe(
 
 
 def test_file_save_failure_binary_dataframe(
-    test_manager: FileManager, dataframe: pd.DataFrame
+    file_manager: FileManager, dataframe: pd.DataFrame
 ) -> None:
-    test_manager.setFilePath("/test/non/existing/path", check_name=False)
-    test_manager.saveDataToBinary(df=dataframe)
+    file_manager.setFilePath("/test/non/existing/path")
+    file_manager.saveDataToBinary(df=dataframe)
     total_path = os.path.join(
-        test_manager.getFilePath(), test_manager.getFileName() + ".pk1"
+        file_manager.getFilePath(), file_manager.getFileName() + ".pk1"
     )
     file_exists = os.path.exists(total_path)
     assert not file_exists
