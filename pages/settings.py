f522c4d0b834ad293e39d08094be1ac6f08d6720
AaronPB <arp629@gmail.com>
2024-09-10 17:24:32 +0200

    Add default config loading and sensor data display

--- pages/settings.py
+++ pages/settings.py
@@ -1,14 +1,39 @@
 import streamlit as st
 import pandas as pd
 
+from src.managers.configManager import ConfigManager
+from src.managers.sensorManager import SensorManager
+from src.managers.fileManager import FileManager
+
+from loguru import logger
+
+
+def newConfigFile(file_path: str):
+    st.session_state.sensor_mngr = SensorManager()
+    st.session_state.file_mngr = FileManager()
+
+    st.session_state.config_mngr.loadConfigFile(file_path)
+    st.session_state.sensor_mngr.setup(st.session_state.config_mngr)
+    if "file_mngr" in st.session_state:
+        st.session_state.file_mngr.setup(st.session_state.config_mngr)
+
 
 def settings_page():
+    # Initialize global instances
+    if "config_mngr" not in st.session_state:
+        logger.info("Defining config manager")
+        st.session_state.config_mngr = ConfigManager()
+    if "sensor_mngr" not in st.session_state:
+        st.session_state.sensor_mngr = SensorManager()
+        st.session_state.sensor_mngr.setup(st.session_state.config_mngr)
+
     st.file_uploader(
         label="Load a custom config file",
         type=".yaml",
         accept_multiple_files=False,
         help="The app will update all sensors and general settings with the new custom configuration.",
     )
+    st.write(f"Loaded config: {st.session_state.config_mngr.getCurrentFilePath()}")
 
     # Test settings
     st.header("Test settings")
@@ -49,31 +74,45 @@ def settings_page():
     connect_col_1, connect_col_2 = st.columns(2)
     connect_col_1.button(label="Connect sensors", type="primary")
 
-    st.error("There is no sensor group information available!")
-
-    # This will be loaded once sensors are connected
-
-    st.write("This will be loaded when parsing sensors from config")
-
-    df = pd.DataFrame(
-        [
-            {"Name": "Loadcell_1", "Connect": True, "Type": "LOADCELL", "Status": "NOT CONNECTED"},
-            {"Name": "Loadcell_2", "Connect": False, "Type": "LOADCELL", "Status": "NOT CONNECTED"},
-            {"Name": "Loadcell_3", "Connect": True, "Type": "LOADCELL", "Status": "NOT CONNECTED"},
-        ]
-    )
-    no_tab, tab2 = st.tabs(["Sensor group", "Example"])
-    with no_tab:
+    sensor_groups = st.session_state.sensor_mngr.getGroups()
+    if not sensor_groups:
         st.error("There is no sensor group information available!")
-    with tab2:
-        st.checkbox(
-            label="Disable entire sensor group",
-            value=False,
-            help="Ignores and does not connect to enabled sensors of this group."
-        )
-        edited_df = st.data_editor(
-            data=df,
-            use_container_width=True,
-            hide_index=True,
-            disabled=("Name", "Type", "Status")
-        )
+    elif len(sensor_groups) == 1:
+        pass
+    else:
+        tab_names = []
+        sensor_info = {}
+        for group in sensor_groups:
+            tab_names.append(group.getName())
+            sensor_info[group.getName()] = {
+                "names": [sensor.getName() for sensor in group.getSensors().values()],
+                "types": [sensor.getType() for sensor in group.getSensors().values()],
+                "read": [sensor.getRead() for sensor in group.getSensors().values()],
+                "status": [
+                    sensor.getStatus() for sensor in group.getSensors().values()
+                ],
+            }
+        tabs = st.tabs(tab_names)
+        for i, tab_name in enumerate(tab_names):
+            df = pd.DataFrame(
+                {
+                    "Connect": sensor_info[tab_name]["read"],
+                    "Name": sensor_info[tab_name]["names"],
+                    "Type": sensor_info[tab_name]["types"],
+                    "Status": sensor_info[tab_name]["status"],
+                }
+            )
+            with tabs[i]:
+                st.checkbox(
+                    label="Disable entire sensor group",
+                    key=f"checkbox_{i}",
+                    value=False,
+                    help="Ignores and does not connect to enabled sensors of this group.",
+                )
+                edited_df = st.data_editor(
+                    data=df,
+                    key=f"edited_df_{i}",
+                    use_container_width=True,
+                    hide_index=True,
+                    disabled=("Name", "Type", "Status"),
+                )
