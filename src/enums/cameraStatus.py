6c4e0482d1f43bd262c3bae63e483bd21781e40a
AaronPB <arp629@gmail.com>
2024-10-07 10:05:04 +0200

    Remove camera recording feature

--- src/enums/cameraStatus.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-from src.enums.qssLabels import QssLabels
-from src.enums.uiResources import IconPaths
-
-
-class CStatus(Enum):
-    IGNORED = (IconPaths.STATUS_OFF, QssLabels.SENSOR_GROUP_IGNORED)
-    AVAILABLE = (IconPaths.STATUS_OK, QssLabels.SENSOR_GROUP_OK)
-    NOT_FOUND = (IconPaths.STATUS_ERROR, QssLabels.SENSOR_GROUP_ERROR)
