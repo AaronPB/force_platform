1f2c4c94982e90ad6f724c4ea9782464b594de74
AaronPB <arp629@gmail.com>
2024-10-15 15:24:32 +0200

    Add color and marker lists to figure traces

--- src/figures/generalFigure.py
+++ src/figures/generalFigure.py
@@ -10,6 +10,25 @@ class GeneralFigure:
         self.x_label = x_label
         self.y_label = y_label
 
+        # Plot style variables
+        self.colors = [
+            "#ff0000",
+            "#00ff00",
+            "#0000ff",
+            "#ffffff",
+            "#ff00cc",
+            "#ffcc00",
+        ]
+        self.markers = [
+            "circle",
+            "square",
+            "diamond",
+            "triangle-up",
+            "hexagon",
+            "y-down",
+        ]
+        self.line_width = 2
+
     def getFigure(
         self, y_values: pd.Series | pd.DataFrame, x_values: pd.Series
     ) -> go.Figure:
@@ -24,14 +43,15 @@ class GeneralFigure:
         if mark_every > 1 and len(x_values) > mark_every:
             marker_indexes = np.arange(0, len(x_values), mark_every)
 
-        for column in y_values.columns:
-            # TODO Change colors and markers styles
+        for i, column in enumerate(y_values.columns):
             self.figure.add_trace(
                 go.Scatter(
                     x=x_values,
                     y=y_values[column],
                     mode="lines",
-                    line=dict(color="#e70f67", width=2),
+                    line=dict(
+                        color=self.colors[i % len(self.colors)], width=self.line_width
+                    ),
                     name=column,
                 )
             )
@@ -41,7 +61,11 @@ class GeneralFigure:
                         x=x_values.iloc[marker_indexes],
                         y=y_values[column].iloc[marker_indexes],
                         mode="markers",
-                        marker=dict(symbol="triangle-up", color="#e70f67", size=10),
+                        marker=dict(
+                            symbol=self.markers[i % len(self.markers)],
+                            color=self.colors[i % len(self.colors)],
+                            size=10,
+                        ),
                         name=column,
                     )
                 )
